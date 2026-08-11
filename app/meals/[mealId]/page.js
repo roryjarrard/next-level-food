0cc9c50d7752674a6699f3182dcc835c8be9da4b
Rory Jarrard <roryjarrard@gmail.com>
2026-08-11 10:48:49 -0600

    display meal details

--- app/meals/[mealId]/page.js
+++ app/meals/[mealId]/page.js
@@ -1,9 +1,36 @@
+import Image from "next/image";
+import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+
 export default async function MealDetails({ params }) {
   const { mealId } = await params;
 
+  const meal = getMeal(mealId);
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
   return (
-    <main>
-      <h1>Meal Details: {mealId}</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} fill alt={`image of ${meal.title}`} />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
   );
 }
